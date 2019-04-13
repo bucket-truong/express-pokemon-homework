3be113c5dab647ec200854f368d4eb81327a7b66
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-04-13 11:32:11 -0600

    connected my database

--- Server.js
+++ Server.js
@@ -3,8 +3,13 @@ const app = express()
 
 const pokemon  = require('./models/pokemon.js')
 
+app.get('/pokemon', (req,res) => {
+  res.send(pokemon)
+})
 
+const port = 3000;
 
 app.listen(3000, () => {
   console.log('app listening', 3000);
 });
+module.exports = app;
