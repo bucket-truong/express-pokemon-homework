4aefc34d58e35a7b6709d868d320f4f2222350f3
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-04-16 22:10:45 -0600

    finished what I could

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const Pokemon  = require('./models/pokemon.js')
 
@@ -8,10 +9,24 @@ app.get('/pokemon', (req,res) => {
   res.render('index.ejs', {pokemon: Pokemon})
 });
 
-app.get('/index/:id', (req, res) => {
+app.get('/pokemon:id', (req, res) => {
   res.render('show.ejs', {pokemon: Pokemon[req.params.id]})
 })
 
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+})
+app.post('/pokemon', (req, res) => {
+  Pokemon.push(req.body)
+  res.send(Pokemon)
+})
+app.delete('./pokemon:id', () => {
+  pokemon.splice(req.params.id, 1)
+  res.redirect('./pokemon')
+} )
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
 app.use('/css', express.static('css'))
 
 const port = 3000;
